f5df33c72c049e04f248e730c8a446a32bcabb41
Sreehari <sreeharibs95@gmail.com>
2019-03-24 09:35:47 +0530

    Modificaions

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-const dom={select:document.querySelector.bind(document),slectAll:document.querySelectorAll.bind(document)};const burger=dom.select('.navbar-burger');const menu=dom.select('.navbar-menu');burger.addEventListener('click',()=>{burger.classList.toggle('is-active');menu.classList.toggle('is-active');});dom.select(`.navbar-menu a[href$="${window.location.pathname}"]`).classList.add('is-active');
+const dom = {
+    select: document.querySelector.bind(document),
+    slectAll: document.querySelectorAll.bind(document)
+};
+const burger = dom.select('.navbar-burger');
+const menu = dom.select('.navbar-menu');
+const item = dom.select('.navbar-item');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+});
+menu.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+});
+dom.select(`.navbar-menu a[href$="${window.location.pathname}"]`).classList.add('is-active');
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- saved from url=(0034)https://sindresorhus.com/#projects -->
 <html lang="en">
 
 <head>
@@ -8,25 +7,24 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#3273DC">
 	<meta name="author" content="Sindre Sorhus">
-	<meta name="description" content="ML &amp; NLP evangelist">
+	<meta name="description" content="Harisree Labs - About Sreehari">
 	<meta property="og:type" content="website">
-	<meta property="og:title" content="Harisree Labs">
-	<meta property="og:url" content="https://harisreelabs.com/">
-	<meta property="og:description" content="ML &amp; NLP evangelist">
-	<meta property="og:image" content="https://harisreelabs.com/logo.jpg">
+	<meta property="og:title" content="About">
+	<meta property="og:url" content="https://harisree.in/about">
+	<meta property="og:description" content="Harisree Labs - Ai &amp; NLP">
+	<meta property="og:image" content="https://harisree.in/logo.jpg">
 	<meta name="twitter:card" content="summary">
-	<meta name="twitter:site" content="@sindresorhus">
-	<meta name="twitter:creator" content="@sindresorhus">
+	<meta name="twitter:site" content="@sreeharibss">
+	<meta name="twitter:creator" content="@sreeharibss">
 	<meta name="x-build-time" content="Fri, 01 Mar 2019 08:01:37 +0000">
-	<title>Sindre Sorhus</title>
-	<link rel="alternate" type="application/rss+xml" title="Sindre Sorhus&#39; blog" href="https://blog.sindresorhus.com/feed">
-	<link rel="preload" href="https://sindresorhus.com/font-awesome.woff" as="font" crossorigin="true">
-	<link rel="icon" href="https://sindresorhus.com/favicon.png" sizes="32x32">
+	<title>Sreehari B S | Harisree Labs</title>
+	<link rel="alternate" type="application/rss+xml" title="Sreehari B S&#39; blog" href="https://medium.com/harisree-labs">
+	<link rel="preload" href="./assets/font-awesome.woff" as="font" crossorigin="true">
+	<link rel="icon" href="./assets/favicon.png" sizes="32x32">
 	<link rel="stylesheet" href="./assets/main.css">
-	<script type="text/javascript" async="" src="./assets/analytics.js"></script>
-	<script type="module" src="https://sindresorhus.com/main.js"></script>
+	<script type="module" src="./assets/main.js"></script>
 	<script src="./assets/timeago.min.js"></script>
-	<script type="module" src="./assets/index.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 
 	<script async="" src="./assets/js"></script>
 	<script>
@@ -47,7 +45,7 @@
 			<div class="container">
 				<nav class="navbar has-shadow is-fixed-top has-background-white" role="navigation" aria-label="main navigation">
 					<div class="navbar-brand">
-						<a id="unicorn-btn" class="navbar-item" href="https://sindresorhus.com/logo.jpg" title="Harisree Labs"><img id="profile-pic" src="./assets/logo.jpg" width="50" height="50" itemprop="image" style="max-height: 50px !important"></a>
+						<a id="unicorn-btn" class="navbar-item" href="#" title="Harisree Labs"><img id="profile-pic" src="./assets/logo.jpg" width="50" height="50" itemprop="image" style="max-height: 50px !important"></a>
 						<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
 							<span aria-hidden="true"></span>
 							<span aria-hidden="true"></span>
@@ -57,9 +55,9 @@
 					<div class="navbar-menu">
 						<div class="navbar-end">
 							<a class="navbar-item is-tab is-active" href="#home">Home</a>
-							<a class="navbar-item is-tab" href="/#articles">Articles</a>
-							<a class="navbar-item is-tab" href="/#works">Works</a>
-							<a class="navbar-item is-tab" href="/about.html">About</a>
+							<a class="navbar-item is-tab" href="#articles">Articles</a>
+							<a class="navbar-item is-tab" href="#works">Works</a>
+							<a class="navbar-item is-tab" href="about.html">About</a>
 						</div>
 					</div>
 				</nav>
@@ -100,23 +98,23 @@ if (location.hash === '#apps') {
 
 						<img id="profile-pic" src="./assets/sreehari-bs.jpg" width="160" height="160" itemprop="image">
 						<h1 class="title is-2" itemprop="name">Sreehari B S</h1>
-						<h2 class="subtitle is-4" itemprop="description">Machine Learning &amp; NLP enthusiast</h2>
-						<p>The only way to learn machine learning is to do machine learning.</p>
+						<h2 class="subtitle is-4" itemprop="description">Machine Learning  enthusiast</h2>
+						<p>Life would be much easier if I had the source code.</p>
 						<br>
 						<p class="has-text-centered">
-							<a class="button is-large is-white" href="https://medium.com/harisree-labs">
+							<a class="button is-large is-white" href="https://medium.com/harisree-labs" target="_blank">
 								<span class="icon">
 									<i class="fa fa-pencil"></i>
 								</span>
 								<span>Blog</span>
 							</a>
-							<a class="button is-large is-white" href="https://github.com/sreeharibs">
+							<a class="button is-large is-white" href="https://github.com/sreeharibs" target="_blank">
 								<span class="icon">
 									<i class="fa fa-github"></i>
 								</span>
 								<span>Code</span>
 							</a>
-							<a class="button is-large is-white" href="https://twitter.com/sreeharibss">
+							<a class="button is-large is-white" href="https://twitter.com/sreeharibss" target="_blank">
 								<span class="icon">
 									<i class="fa fa-twitter"></i>
 								</span>
@@ -124,16 +122,36 @@ if (location.hash === '#apps') {
 							</a>
 						</p>
 						<br>
-						<a href="https://sindresorhus.com/thanks">Huge thanks to all my amazing supporters!</a>
+						<a href="#">Huge thanks to all my amazing supporters!</a>
 						<br>
 						<br>
 						<hr id="latest-blog-post-separator">
 						<div id="latest-blog-post" class="visible">
 							<h4 class="title is-4">
-								<a class="is-gray-link" href="https://medium.com/@sindresorhus/small-focused-modules-9238d977a92a">Small Focused Modules</a>
+
+								<?php
+									$json = file_get_contents('https://medium.com/harisree-labs/latest?format=json');
+
+									$re = '/.*({"success":)/m';
+									$json = preg_replace($re, '$1', $json);
+
+									$json = json_decode($json, true);
+									$url_base = 'https://medium.com/harisree-labs/';
+
+									$i = 0;
+
+									$title = $json['payload']['posts'][$i]['title'];
+									$subtitle = $json['payload']['posts'][$i]['virtuals']['subtitle'];
+									$uniqueSlug = $json['payload']['posts'][$i]['uniqueSlug'];
+									$tag = $json['payload']['posts'][$i]['virtuals']['tags'][0]['name'];
+
+								?>
+
+
+								<a class="is-gray-link" href="<?php echo $url_base.$uniqueSlug;?>" target="_blank"><?php echo $title;?></a>
 								<span class="tag is-small">Latest writing</span>
 							</h4>
-							<p>Make small focused modules for reusability and to make it possible to build larger more advanced things that are easier to reason about.</p>
+							<p><?php echo $subtitle;?></p>
 						</div>
 					</div>
 				</div>
@@ -151,35 +169,27 @@ if (location.hash === '#apps') {
 			</div>
 		</div>
 	</section>
-	<section class="hero is-fullheight is-gray">
+	<section class="hero is-fullheight">
 		<div id="articles" class="hero-body container">
 			<div class="columns is-vcentered">
 				<div class="column has-text-centered">
 					<img id="unicorn-icon" src="./assets/logo.jpg" width="100" height="100">
 					<h4 class="title is-4" style="font-weight: 400">Medium</h4>
 					<div id="latest-commit">
-						<a class="commit-title nice-link" href="https://github.com/sindresorhus/quora-unblocker-userscript/commit/d635fe41cc84d4ef9f742c120c7b40ce97c04565"
-							title="See https://github.com/sindresorhus/legal-notices/blob/master/2019-03-01-quora.md">Handle Quora takedown notice</a>
+						<a class="commit-title nice-link" href="https://medium.com/harisree-labs/">View All Articles</a>
 						<br>
 						<span style="font-size: 12px">
-							<span class="commit-date">13 hours ago</span> in <a class="repo-title" href="https://github.com/sindresorhus/quora-unblocker-userscript">quora-unblocker-userscript</a>
+							<!-- <span class="commit-date">13 hours ago</span> in <a class="repo-title" href="https://github.com/sindresorhus/quora-unblocker-userscript">quora-unblocker-userscript</a> -->
 						</span>
 					</div>
 					<br>
-					<a href="https://gitstalk.netlify.com/sindresorhus">Stalk me</a>
+					<a href="https://facebook.com/sreeharibs95">Follow me</a>
 				</div>
 				<div id="latest-repos-container" class="column is-three-fifths is-offset-1 has-nice-link">
 					<h2 class="title is-2">Latest articles</h2>
 					<template id="latest-repos-template"></template>
 					<div id="latest-repos" class="columns is-multiline">
 						<?php
-							$json = file_get_contents('https://medium.com/harisree-labs/latest?format=json');
-
-							$re = '/.*({"success":)/m';
-							$json = preg_replace($re, '$1', $json);
-
-							$json = json_decode($json, true);
-							$url_base = 'https://medium.com/harisree-labs/';
 
 							$i = 0;
 							while (isset($json['payload']['posts'][$i]) && $i < 6) {
@@ -191,7 +201,7 @@ if (location.hash === '#apps') {
 
 								echo '<div class="column is-half">
 											<h4 class="title is-4">
-											<a class="latest-repos-title" href="'.$url_base.$uniqueSlug.'">'.$title.'</a>
+											<a class="latest-repos-title" href="'.$url_base.$uniqueSlug.'" target="_blank">'.$title.'</a>
 											<span class="tag is-small latest-repos-language" style="color: rgb(51, 51, 51); background-color: rgb(241, 224, 90);">'.$tag.'</span>
 											</h4>
 											<p class="latest-repos-description">'.$subtitle.'</p>
@@ -211,154 +221,54 @@ if (location.hash === '#apps') {
 		<div id="works" class="container">
 			<br /><br /><br />
 			<h2 class="title is-2">Works</h2>
-			<p>Some of my showcasable works. Excus</p>
+			<p>I am in the middle of tuning up my works to a demonstrable form. Meanwhile you can browse through my github repository. </p>
 			<br>
 			<div class="columns is-multiline">
 				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/gifski">
+					<a href="https://github.com/sreeharibs/Review-Analysis">
 						<div class="box">
 							<figure class="image">
-
-								<img src="./assets/thumbnail.jpg">
+								<img src="./assets/sentiment.jpg">
 							</figure>
 							<h2 class="title">
-								<span>Gifski</span>
-								<span class="tag is-small is-success">New!</span>
-								<span class="tag is-small">macOS</span>
+								<span>Review</span>
+								<span class="tag is-small">GitHub</span>
 							</h2>
-							<p class="subtitle is-6">Convert videos to high-quality GIFs</p>
+							<p class="subtitle is-6">
+								Sentimental analysis of mobile phone reviews from amazon.
+							</p>
 						</div>
 					</a>
 				</div>
 				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/kap">
+					<a href="https://github.com/harisree-labs/fastText">
 						<div class="box">
 							<figure class="image">
-
-								<img src="./assets/thumbnail(1).jpg">
+								<img src="./assets/fasttext.png">
 							</figure>
 							<h2 class="title">
-								<span>Kap</span>
-								<span class="tag is-small">macOS</span>
+								<span>Fast Text</span>
+								<span class="tag is-small">GitHub</span>
 							</h2>
-							<p class="subtitle is-6">Record your screen</p>
+							<p class="subtitle is-6">
+								Modificatons to improve malayalam word vector's accuracy.
+							</p>
 						</div>
 					</a>
 				</div>
 				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/black-out">
+					<a href="https://github.com/harisree-labs/">
 						<div class="box">
 							<figure class="image">
-								<img src="./assets/thumbnail(2).jpg">
-
+								<img src="./assets/github.jpg">
 							</figure>
 							<h2 class="title">
-								<span>Black Out</span>
-								<span class="tag is-small">macOS</span>
+								<span>Github</span>
+								<span class="tag is-small">GitHub</span>
 							</h2>
-							<p class="subtitle is-6">Hide sensitive parts of an image</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/heic-converter">
-						<div class="box">
-							<figure class="image">
-								<img src="./assets/thumbnail(3).jpg">
-							</figure>
-							<h2 class="title">
-								<span>HEIC Converter</span>
-								<span class="tag is-small">macOS</span>
-							</h2>
-							<p class="subtitle is-6">Convert HEIC images to JPEG or PNG</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/shareful">
-						<div class="box">
-							<figure class="image">
-								<img src="./assets/thumbnail(4).jpg">
-							</figure>
-							<h2 class="title">
-								<span>Shareful</span>
-								<span class="tag is-small is-info">Prerelease</span>
-								<span class="tag is-small">macOS</span>
-							</h2>
-							<p class="subtitle is-6">Give the macOS Share menu superpowers</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/lungo">
-						<div class="box">
-							<figure class="image">
-
-								<img src="./assets/thumbnail(5).jpg">
-							</figure>
-							<h2 class="title">
-								<span>Lungo</span>
-								<span class="tag is-small">macOS</span>
-							</h2>
-							<p class="subtitle is-6">Prevent your Mac from going to sleep</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/touch-bar-simulator">
-						<div class="box">
-							<figure class="image">
-
-								<img src="./assets/thumbnail(6).jpg">
-							</figure>
-							<h2 class="title">
-								<span>Touch Bar Simulator</span>
-								<span class="tag is-small">macOS</span>
-							</h2>
-							<p class="subtitle is-6">Use the Touch Bar on any Mac</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/battery-indicator">
-						<div class="box">
-							<figure class="image">
-								<img src="./assets/thumbnail(7).jpg">
-
-							</figure>
-							<h2 class="title">
-								<span>Battery Indicator</span>
-								<span class="tag is-small">macOS</span>
-							</h2>
-							<p class="subtitle is-6">Remaining battery time in your menu bar</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/caprine">
-						<div class="box">
-							<figure class="image">
-								<img src="./assets/thumbnail(8).jpg">
-							</figure>
-							<h2 class="title">
-								<span>Caprine</span>
-								<span class="tag is-small">Cross-platform</span>
-							</h2>
-							<p class="subtitle is-6">Elegant Facebook Messenger desktop app</p>
-						</div>
-					</a>
-				</div>
-				<div class="column is-mobile is-half-tablet is-one-third-desktop">
-					<a href="https://sindresorhus.com/blear">
-						<div class="box">
-							<figure class="image">
-								<img src="./assets/thumbnail(9).jpg">
-							</figure>
-							<h2 class="title">
-								<span>Blear</span>
-								<span class="tag is-small">iOS</span>
-							</h2>
-							<p class="subtitle is-6">Transform photos into blurry wallpapers</p>
+							<p class="subtitle is-6">
+								 Explore my github repository
+							</p>
 						</div>
 					</a>
 				</div>
@@ -369,6 +279,11 @@ if (location.hash === '#apps') {
 
 
 	<script>
+
+	$(document).ready(function(){
+	  alert("Hi");
+	});
+
 	</script>
 
 </body>
